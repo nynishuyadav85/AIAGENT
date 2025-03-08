99b46cf65b36362b8ef2601dbb53de3c03133208
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-08 14:04:34 +0530

    JS executor tool

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -16,6 +16,42 @@ const weatherTool = tool(async ({ query }) => {
         })
     }
 )
+
+async function evalAndCaptureOutput(code) {
+    const oldLog = console.log;
+    const oldError = console.error;
+
+    const output = [];
+    let errorOutput = [];
+
+    console.log = (...args) => output.push(args.join(' '));
+    console.error = (...args) => errorOutput.push(args.join(' '));
+
+    try {
+        await eval(code);
+    } catch (error) {
+        errorOutput.push(error.message);
+    }
+
+    console.log = oldLog;
+    console.error = oldError;
+
+    return { stdout: output.join('\n'), stderr: errorOutput.join('\n') };
+}
+
+const jsExecutor = tool(async ({ code }) => {
+    const result = await evalAndCaptureOutput(code)
+    return result
+}, {
+    name: "js",
+    description: `Run general purpose javascript code. 
+      This can be used to access Internet or do any computation that you need. 
+      The output will be composed of the stdout and stderr. 
+      The code should be written in a way that it can be executed with javascript eval in node environment.`,
+    schema: z.object({
+        code: z.string()?.describe("Code to be excuted")
+    })
+})
 const model = new ChatAnthropic({
     model: 'claude-3-5-sonnet-latest',
 })
@@ -23,7 +59,7 @@ const checkpointSaver = new MemorySaver()
 
 export const agent = createReactAgent({
     llm: model,
-    tools: [weatherTool],
+    tools: [weatherTool, jsExecutor],
     checkpointSaver
 })
 
