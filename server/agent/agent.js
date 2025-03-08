0ab0ba0867ebf4097c31a6b703c3b3c4b8a25d70
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-08 12:27:08 +0530

    AIAGENT with REST Api with express

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -6,7 +6,6 @@ import { MemorySaver } from '@langchain/langgraph';
 
 
 const weatherTool = tool(async ({ query }) => {
-    console.log('querying weather', query)
     return 'The weather in tokoyo is sunny'
 },
     {
@@ -21,34 +20,10 @@ const model = new ChatAnthropic({
     model: 'claude-3-5-sonnet-latest',
 })
 const checkpointSaver = new MemorySaver()
-const agent = createReactAgent({
+
+export const agent = createReactAgent({
     llm: model,
     tools: [weatherTool],
     checkpointSaver
 })
 
-
-const result = await agent.invoke({
-    messages: [
-        {
-            role: 'user',
-            content: 'What is the weather in Tokyo?'
-        }
-    ]
-}, {
-    configurable: { thread_id: 42 }
-})
-
-const followUp = await agent.invoke({
-    messages: [
-        {
-            role: 'user',
-            content: 'What city is this about?'
-        }
-    ]
-}, {
-    configurable: { thread_id: 42 }
-})
-
-console.log(result.messages.at(-1)?.content);
-console.log("followUp : " + followUp.messages.at(-1)?.content);
---
+++ server/agent/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import { agent } from "./agent.js";
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cors({ origin: '*' }))
+
+app.post("/generate", async (req, res) => {
+    const { prompt, thread_id } = req.body
+    const result = await agent.invoke({
+        messages: [
+            {
+                role: 'user',
+                content: prompt
+            }
+        ]
+    }, {
+        configurable: { thread_id: thread_id }
+    })
+
+    res.json(result.messages.at(-1).content)
+})
+
+app.listen(port, () => {
+    console.log(`AIAGENT app listening at http://localhost:${port}`);
+}
+);
